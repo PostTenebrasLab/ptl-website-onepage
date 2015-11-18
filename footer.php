ad0f6fc53c5bf07946e72995c2542edb5fefc0da
lukrate <boris@geneva-3d.com>
2015-11-18 13:01:03 +0100

    footer friends ok

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
 <footer>
-	<p id="official-data">Post Tenebras Lab</p>
+	<a href=""><img src="img/friends/fixme.jpg"/></a>
+	<a href=""><img src="img/friends/lemanmake.jpg"/></a>
+	<a href=""><img src="img/friends/museomix.jpg"/></a>
+	<a href=""><img src="img/friends/gull.jpg"/></a>
+	<a href=""><img src="img/friends/fetonslinux.jpg"/></a>
+	<a href=""><img src="img/friends/pangloss.jpg"/></a>
 </footer>
