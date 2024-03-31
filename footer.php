f498a210f924cdaef703028ce58b3f1a80e4fd25
Martin Voelkle <martin.voelkle@gmail.com>
2024-03-31 14:23:58 +0200

    static

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-	<a href="https://fixme.ch/"><img src="img/friends/fixme.jpg" alt="logo fixme"/></a>
-	<a href="http://www.lemanmake.com/"><img src="img/friends/lemanmake.jpg" alt="logo lemanmake"/></a>
-	<a href="http://www.museomix.org/"><img src="img/friends/museomix.jpg" alt="logo museomix"/></a>
-	<a href="https://www.linux-gull.ch/"><img src="img/friends/gull.jpg" alt="logo gull"/></a>
-	<a href="https://panglosslabs.org/"><img src="img/friends/pangloss.jpg" alt"logo pangloss labs"/></a>
-	<a href="https://init7.net/"><img src="img/friends/init7-logo-rgb.png" alt="logo init7"/></a>
-</footer>
